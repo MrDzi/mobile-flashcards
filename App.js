76329cd02bd9911be677101d571b01b71ca90555
Dejan <dejanhadziomerovic@gmail.com>
2017-09-27 22:11:47 +0200

    Set up first components

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import { Constants } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import DecksList from './components/DecksList';
+import AddDeck from './components/AddDeck';
+
+const MFStatusBar = ({backgroundColor, ...props}) => (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+        <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+)
+
+const Tabs = TabNavigator({
+    DecksList: {
+        screen: DecksList,
+        navigationOptions: {
+            tabBarLabel: 'Decks List',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-list' size={34} color={tintColor} />
+        }
+    },
+    AddDeck: {
+        screen: AddDeck,
+        navigationOptions: {
+            tabBarLabel: 'Add New Deck',
+            tabBarIcon: ({ tintColor }) => <Ionicons name='ios-add-circle-outline' size={26} color={tintColor} />
+        }
+    }
+});
 
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
+            <View style={{flex: 1}}>
+                <MFStatusBar backgroundColor="purple" barStyle="light-content" />
+                <Tabs style={styles.container} />
             </View>
         );
     }
